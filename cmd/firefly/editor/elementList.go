d9dfa158c67e618088257217ecba8a193c973c49
Jannis Rieger <omniskopus@gmail.com>
2020-03-25 03:17:44 +0100

    fixed selection bugs

--- cmd/firefly/editor/elementList.go
+++ cmd/firefly/editor/elementList.go
@@ -2,7 +2,10 @@ package editor
 
 import (
 	"github.com/omniskop/firefly/pkg/project"
+	"github.com/sirupsen/logrus"
 )
+
+const logSelectionChanges bool = false
 
 // elementList contains a modifiable list of graphicItems
 // onChange will be called whenever the list changes and could potentially be made optional if that is required
@@ -34,12 +37,27 @@ func (s elementList) copyElements() []*project.Element {
 }
 
 func (s *elementList) removeIfFound(searched *elementGraphicsItem) {
+	if len(s.elements) == 0 {
+		return
+	} else if len(s.elements) == 1 {
+		if s.elements[0] == searched {
+			searched.hideHandles()
+			s.elements = s.elements[:0] // set length to zero but preserve capacity
+		}
+		return
+	}
+
 	for i, element := range s.elements {
 		if element == searched {
 			element.hideHandles()
 			s.elements[i] = s.elements[len(s.elements)-1]
-			s.elements = s.elements[:len(s.elements)-2]
+			s.elements = s.elements[:len(s.elements)-1]
+			break
 		}
+	}
+
+	if logSelectionChanges {
+		logrus.WithFields(logrus.Fields{"len": len(s.elements), "func": "removeIfFound"}).Debug("selection changed")
 	}
 	s.onChange()
 }
@@ -49,17 +67,29 @@ func (s *elementList) clear() {
 		element.hideHandles()
 	}
 	s.elements = []*elementGraphicsItem{}
+
+	if logSelectionChanges {
+		logrus.WithFields(logrus.Fields{"len": len(s.elements), "func": "clear"}).Debug("selection changed")
+	}
 	s.onChange()
 }
 
 func (s *elementList) add(item *elementGraphicsItem) {
 	item.showHandles()
 	s.elements = append(s.elements, item)
+
+	if logSelectionChanges {
+		logrus.WithFields(logrus.Fields{"len": len(s.elements), "func": "add"}).Debug("selection changed")
+	}
 	s.onChange()
 }
 
 func (s *elementList) set(item *elementGraphicsItem) {
 	s.clear()
 	s.add(item)
+
+	if logSelectionChanges {
+		logrus.WithFields(logrus.Fields{"len": len(s.elements), "func": "set"}).Debug("selection changed")
+	}
 	s.onChange()
 }
